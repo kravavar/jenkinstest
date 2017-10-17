4c34221949c974bcf887b2f009ad9038e72985e8
kravavar <user@user-PC>
2017-10-17 21:51:22 +0530

    First program commit

---
+++ MavenTest/src/com/jenkins/test/JenkinsTest.java
@@ -0,0 +1,11 @@
+package com.jenkins.test;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hey thi is my first Devops test for git and jenkins CI and CD");
+	}
+
+}
